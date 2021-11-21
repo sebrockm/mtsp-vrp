1692233d524a2356701a0cd8d3dd0a8445dfc6e2
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2021-11-21 11:36:01 +0100

    Use GenerateExportHeader

--- mtsp-vrp-c/include/mtsp-vrp-c.h
+++ mtsp-vrp-c/include/mtsp-vrp-c.h
@@ -1,9 +1,11 @@
 #ifndef MTSP_VRP_C_H
 #define MTSP_VRP_C_H
 
+#include "mtsp-vrp-c_export.h"
+
 extern "C"
 {
-    int solve_mtsp_vrp(size_t numberOfAgents, size_t numberOfNodes, const int* start_positions, const int* end_positions, const double* weights,
+    MTSP_VRP_C_EXPORT int solve_mtsp_vrp(size_t numberOfAgents, size_t numberOfNodes, const int* start_positions, const int* end_positions, const double* weights,
         double* lowerBound, double* upperBound, int* paths, size_t* pathOffsets);
 }
 

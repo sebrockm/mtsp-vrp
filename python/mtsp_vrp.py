28955855d253f51fcb9397a0b22c6774f66f8d55
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2022-05-25 23:42:56 +0200

    Move python binding to common file (#25)
    * Extract mtsp-vrp python function into module
    
    * Move wrapper function to module

---
+++ python/mtsp_vrp.py
@@ -0,0 +1,54 @@
+from ctypes import *
+import numpy as np
+from numpy.ctypeslib import ndpointer
+from os import path
+
+with open(path.join(path.dirname(path.abspath(__file__)), '_mtsp_vrp_c_lib_path.txt')) as f:
+    _mtsp_vrp_c_lib_path = f.readline()
+
+_solve_mtsp_vrp = cdll.LoadLibrary(_mtsp_vrp_c_lib_path).solve_mtsp_vrp
+_solve_mtsp_vrp.restype = c_int
+_solve_mtsp_vrp.argtypes = [
+    c_size_t, # numberOfAgents
+    c_size_t, # numberOfNodes
+    ndpointer(c_size_t, flags='C_CONTIGUOUS'), # start_positions
+    ndpointer(c_size_t, flags='C_CONTIGUOUS'), # end_positions
+    ndpointer(c_int, flags='C_CONTIGUOUS'), # weights
+    c_int, # timeout
+    c_size_t, # numberOfThreads
+    POINTER(c_double), # lowerBound
+    POINTER(c_double), # upperBound
+    ndpointer(c_size_t, flags='C_CONTIGUOUS'), # paths
+    ndpointer(c_size_t, flags='C_CONTIGUOUS') # pathOffsets
+]
+
+def solve_mtsp_vrp(start_positions, end_positions, weights, timeout):
+    A = len(start_positions)
+    N = len(weights)
+    start_positions = np.array(start_positions, dtype=np.uint64)
+    end_positions = np.array(end_positions, dtype=np.uint64)
+    weights = np.array(weights, dtype=np.int32)
+    number_of_threads = 0
+    lb = c_double(0)
+    ub = c_double(0)
+    pathsBuffer = np.zeros(shape=(N,), dtype=np.uint64)
+    offsets = np.zeros(shape=(A,), dtype=np.uint64)
+
+    result = _solve_mtsp_vrp(A, N, start_positions, end_positions, weights, timeout, number_of_threads, byref(lb), byref(ub), pathsBuffer, offsets)
+    if result < 0:
+        print(f'error: {result}')
+        return None, None, result, result
+
+    paths = []
+    lengths = []
+    for a in range(A):
+        start = offsets[a]
+        end = offsets[a+1] if a+1 < A else N
+        path = np.array(pathsBuffer[start:end])
+        length = np.sum(weights[path[:-1], path[1:]])
+        if start_positions[a] == end_positions[a]:
+            length += weights[path[-1], path[0]]
+        paths.append(path)
+        lengths.append(length)
+
+    return paths, lengths, lb.value, ub.value
--- python/tsplib/tsplib.py
+++ python/tsplib/tsplib.py
@@ -1,6 +1,5 @@
 import tsplib95 as tsplib
 import numpy as np
-from numpy.ctypeslib import ndpointer
 from ctypes import *
 import json
 import os
@@ -8,70 +7,13 @@ import sys
 import time
 from tqdm import tqdm
 
-solve_mtsp_vrp = None
+if __name__ == '__main__':
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from mtsp_vrp import solve_mtsp_vrp
 
-def timing(f):
-    def wrap(*args, **kwargs):
-        time1 = time.time()
-        ret = f(*args, **kwargs)
-        time2 = time.time()
 
-        return ret, time2 - time1
-    return wrap
-
-@timing
-def solve_mtsp(start_positions, end_positions, weights, timeout):
-    A = len(start_positions)
-    N = len(weights)
-    start_positions = np.array(start_positions, dtype=np.uint64)
-    end_positions = np.array(end_positions, dtype=np.uint64)
-    weights = np.array(weights, dtype=np.int32)
-    number_of_threads = 0
-    lb = c_double(0)
-    ub = c_double(0)
-    pathsBuffer = np.zeros(shape=(N,), dtype=np.uint64)
-    offsets = np.zeros(shape=(A,), dtype=np.uint64)
-
-    result = solve_mtsp_vrp(A, N, start_positions, end_positions, weights, timeout, number_of_threads, byref(lb), byref(ub), pathsBuffer, offsets)
-    if result < 0:
-        print(f'error: {result}')
-        return None, None, result, result
-
-    print(pathsBuffer)
-    print(offsets)
-
-    paths = []
-    lengths = []
-    for a in range(A):
-        start = offsets[a]
-        end = offsets[a+1] if a+1 < A else N
-        path = np.array(pathsBuffer[start:end])
-        length = np.sum(weights[path[:-1], path[1:]])
-        if start_positions[a] == end_positions[a]:
-            length += weights[path[-1], path[0]]
-        paths.append(path)
-        lengths.append(length)
-
-    return paths, lengths, lb.value, ub.value
-
-def main(dll_path, timeout_ms):
-    global solve_mtsp_vrp
-    solve_mtsp_vrp = cdll.LoadLibrary(dll_path).solve_mtsp_vrp
-    solve_mtsp_vrp.restype = c_int
-    solve_mtsp_vrp.argtypes = [
-        c_size_t, # numberOfAgents
-        c_size_t, # numberOfNodes
-        ndpointer(c_size_t, flags='C_CONTIGUOUS'), # start_positions
-        ndpointer(c_size_t, flags='C_CONTIGUOUS'), # end_positions
-        ndpointer(c_int, flags='C_CONTIGUOUS'), # weights
-        c_int, # timeout
-        c_size_t, # numberOfThreads
-        POINTER(c_double), # lowerBound
-        POINTER(c_double), # upperBound
-        ndpointer(c_size_t, flags='C_CONTIGUOUS'), # paths
-        ndpointer(c_size_t, flags='C_CONTIGUOUS') # pathOffsets
-    ]
-    base = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tsplib')
+def main(timeout_ms):
+    base = os.path.dirname(os.path.abspath(__file__))
     files = [os.path.join(base, kind, f) for kind in ['sop', 'atsp', 'tsp'] for f in os.listdir(os.path.join(base, kind))]
 
     with open(os.path.join(base, 'best-known-solutions.json')) as f:
@@ -124,7 +66,9 @@ def main(dll_path, timeout_ms):
                         weights[i, j] = P.get_weight(nodes[i], nodes[j])
 
             print(f'starting solving {f} ...')
-            (paths, lengths, lb, ub), seconds = solve_mtsp(start_positions=[0], end_positions=[0], weights=weights, timeout=timeout_ms)
+            star_time = time.time()
+            paths, lengths, lb, ub = solve_mtsp_vrp(start_positions=[0], end_positions=[0], weights=weights, timeout=timeout_ms)
+            seconds = time.time() - star_time
             if paths is None:
                 print('solve_mtsp error:', lb)
                 result_string = f'{base_name:<15s} N={N:>5d} A=1 mode=sum time=-------s result=-------- gap=-------\n'
@@ -147,4 +91,4 @@ def main(dll_path, timeout_ms):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], int(sys.argv[2]))
+    main(int(sys.argv[1]))

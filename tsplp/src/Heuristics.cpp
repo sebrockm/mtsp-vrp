f03e7e7c6b6ddaeec4ed5bb175d85e4fb295dd9b
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2023-03-29 00:00:04 +0200

    Unify two-opt implementations

--- tsplp/src/Heuristics.cpp
+++ tsplp/src/Heuristics.cpp
@@ -280,14 +280,82 @@ std::tuple<std::vector<std::vector<size_t>>, double> NearestInsertion(
     }
 }
 
-std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
-    std::vector<std::vector<size_t>> paths, xt::xarray<double> weights,
-    const DependencyGraph& dependencies, std::chrono::steady_clock::time_point endTime)
+class PathsInfo
+{
+    OptimizationMode m_optimizationMode;
+    size_t m_longestA = 0;
+    std::vector<double> m_pathLengths;
+
+public:
+    PathsInfo(
+        OptimizationMode optimizationMode, const std::vector<std::vector<size_t>>& paths,
+        const xt::xarray<double> weights)
+        : m_optimizationMode(optimizationMode)
+    {
+        if (m_optimizationMode == OptimizationMode::Max)
+        {
+            const auto A = paths.size();
+            m_pathLengths.resize(A);
+            for (size_t a = 0; a < A; ++a)
+            {
+                m_pathLengths[a] = CalculatePathLength(paths[a], weights);
+
+                if (m_pathLengths[a] > m_pathLengths[m_longestA])
+                    m_longestA = a;
+            }
+        }
+    }
+
+    [[nodiscard]] std::array<size_t, 2> GetA2Range() const
+    {
+        return m_optimizationMode == OptimizationMode::Max
+            ? std::array<size_t, 2> { m_longestA, m_longestA + 1 }
+            : std::array<size_t, 2> { 0, m_pathLengths.size() };
+    }
+
+    [[nodiscard]] double ApplyImprovement(
+        size_t a1, double improvementA1, size_t a2, double improvementA2)
+    {
+        if (m_optimizationMode == OptimizationMode::Sum)
+            return improvementA1 + improvementA2;
+
+        const auto oldObjective = m_pathLengths[m_longestA];
+        double newObjective = 0.0;
+        size_t newLongestA = 0;
+        for (size_t a = 0; a < m_pathLengths.size(); ++a)
+        {
+            auto aLength = m_pathLengths[a];
+            if (a == a1)
+                aLength -= improvementA1;
+            if (a == a2)
+                aLength -= improvementA2;
+
+            if (aLength > newObjective)
+            {
+                newLongestA = a;
+                newObjective = aLength;
+            }
+        }
+
+        m_pathLengths[a1] -= improvementA1;
+        m_pathLengths[a2] -= improvementA2;
+        m_longestA = newLongestA;
+
+        return oldObjective - newObjective;
+    }
+};
+
+std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPaths(
+    OptimizationMode optimizationMode, std::vector<std::vector<size_t>> paths,
+    xt::xarray<double> weights, const DependencyGraph& dependencies,
+    std::chrono::steady_clock::time_point endTime)
 {
     assert(weights.dimension() == 2);
 
     const auto A = paths.size();
     assert(A > 0);
+
+    PathsInfo pathsInfo(optimizationMode, paths, weights);
 
     bool hasImproved = true;
     double improvementSum = 0.0;
@@ -298,7 +366,8 @@ std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
 
         for (size_t a1 = 0; a1 < A; ++a1)
         {
-            for (size_t a2 = 0; a2 < A; ++a2)
+            const auto [a2Start, a2End] = pathsInfo.GetA2Range();
+            for (size_t a2 = a2Start; a2 < a2End; ++a2)
             {
                 for (size_t i = 1; i < paths[a1].size() - 1; ++i)
                 {
@@ -332,7 +401,8 @@ std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
                                 continue;
                         }
 
-                        double improvement = 0.0;
+                        double improvementA1 = 0.0;
+                        double improvementA2 = 0.0;
 
                         if (a1 == a2 && j == i + 1)
                         {
@@ -343,7 +413,7 @@ std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
                             const auto after1  = weights(paths[a1][i - 1], paths[a1][i + 1]);
                             const auto after2  = weights(paths[a1][i + 1], paths[a1][i    ]);
                             const auto after4  = weights(paths[a1][i    ], paths[a1][i + 2]);
-                            improvement = before1 + before2 + before4 - after1 - after2 - after4;
+                            improvementA1 = before1 + before2 + before4 - after1 - after2 - after4;
                             // clang-format on
                         }
                         else
@@ -357,15 +427,17 @@ std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
                             const auto after2  = weights(paths[a2][j    ], paths[a1][i + 1]);
                             const auto after3  = weights(paths[a2][j - 1], paths[a1][i    ]);
                             const auto after4  = weights(paths[a1][i    ], paths[a2][j + 1]);
-                            const auto a1Imp = before1 + before2 - after1 - after2;
-                            const auto a2Imp = before3 + before4 - after3 - after4;
-                            improvement = a1Imp + a2Imp;
+                            improvementA1 = before1 + before2 - after1 - after2;
+                            improvementA2 = before3 + before4 - after3 - after4;
                             // clang-format on
                         }
 
                         assert(weights(paths[a1][i], paths[a1][i]) == 0);
 
-                        if (improvement > 0.0)
+                        const auto improvement
+                            = pathsInfo.ApplyImprovement(a1, improvementA1, a2, improvementA2);
+
+                        if (improvement > 0)
                         {
                             hasImproved = true;
                             std::swap(paths[a1][i], paths[a2][j]);
@@ -378,151 +450,6 @@ std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsSum(
     }
 
     return { paths, improvementSum };
-}
-
-std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPathsMax(
-    std::vector<std::vector<size_t>> paths, xt::xarray<double> weights,
-    const DependencyGraph& dependencies, std::chrono::steady_clock::time_point endTime)
-{
-    assert(weights.dimension() == 2);
-
-    const auto A = paths.size();
-    assert(A > 0);
-
-    size_t longestA = 0;
-    std::vector<double> pathLengths(A);
-    for (size_t a = 0; a < A; ++a)
-    {
-        pathLengths[a] = CalculatePathLength(paths[a], weights);
-
-        if (pathLengths[a] > pathLengths[longestA])
-            longestA = a;
-    }
-
-    bool hasImproved = true;
-    double improvementSum = 0.0;
-
-    while (hasImproved && std::chrono::steady_clock::now() < endTime)
-    {
-        hasImproved = false;
-
-        for (size_t a1 = 0; a1 < A; ++a1)
-        {
-            const auto a2 = longestA;
-            for (size_t i = 1; i < paths[a1].size() - 1; ++i)
-            {
-                const auto jStart = a1 == a2 ? i + 1 : static_cast<size_t>(1);
-                for (size_t j = jStart; j < paths[a2].size() - 1; ++j)
-                {
-                    const auto u = paths[a1][i];
-                    const auto v = paths[a2][j];
-
-                    if (a1 != a2
-                        && (!dependencies.GetIncomingSpan(u).empty()
-                            || !dependencies.GetIncomingSpan(v).empty()
-                            || !dependencies.GetOutgoingSpan(u).empty()
-                            || !dependencies.GetOutgoingSpan(v).empty()))
-                        continue;
-
-                    if (a1 == a2)
-                    {
-                        bool wouldBreakDependency = false;
-                        for (auto k = i; k < j; ++k)
-                        {
-                            if (dependencies.HasArc(u, paths[a1][k + 1])
-                                || dependencies.HasArc(paths[a1][k], v))
-                            {
-                                wouldBreakDependency = true;
-                                break;
-                            }
-                        }
-
-                        if (wouldBreakDependency)
-                            continue;
-                    }
-
-                    double improvementA1 = 0.0;
-                    double improvementA2 = 0.0;
-
-                    if (a1 == a2 && j == i + 1)
-                    {
-                        // clang-format off
-                        const auto before1 = weights(paths[a1][i - 1], paths[a1][i    ]);
-                        const auto before2 = weights(paths[a1][i    ], paths[a1][i + 1]);
-                        const auto before4 = weights(paths[a1][i + 1], paths[a1][i + 2]);
-                        const auto after1  = weights(paths[a1][i - 1], paths[a1][i + 1]);
-                        const auto after2  = weights(paths[a1][i + 1], paths[a1][i    ]);
-                        const auto after4  = weights(paths[a1][i    ], paths[a1][i + 2]);
-                        improvementA1 = before1 + before2 + before4 - after1 - after2 - after4;
-                        // clang-format on
-                    }
-                    else
-                    {
-                        // clang-format off
-                        const auto before1 = weights(paths[a1][i - 1], paths[a1][i    ]);
-                        const auto before2 = weights(paths[a1][i    ], paths[a1][i + 1]);
-                        const auto before3 = weights(paths[a2][j - 1], paths[a2][j    ]);
-                        const auto before4 = weights(paths[a2][j    ], paths[a2][j + 1]);
-                        const auto after1  = weights(paths[a1][i - 1], paths[a2][j    ]);
-                        const auto after2  = weights(paths[a2][j    ], paths[a1][i + 1]);
-                        const auto after3  = weights(paths[a2][j - 1], paths[a1][i    ]);
-                        const auto after4  = weights(paths[a1][i    ], paths[a2][j + 1]);
-                        improvementA1 = before1 + before2 - after1 - after2;
-                        improvementA2 = before3 + before4 - after3 - after4;
-                        // clang-format on
-                    }
-
-                    assert(weights(paths[a1][i], paths[a1][i]) == 0);
-
-                    const auto oldObjective = pathLengths[longestA];
-                    double newObjective = 0.0;
-                    size_t newLongestA = 0;
-                    for (size_t a = 0; a < A; ++a)
-                    {
-                        auto aLength = pathLengths[a];
-                        if (a == a1)
-                            aLength -= improvementA1;
-                        if (a == a2)
-                            aLength -= improvementA2;
-
-                        if (aLength > newObjective)
-                        {
-                            newLongestA = a;
-                            newObjective = aLength;
-                        }
-                    }
-
-                    if (newObjective < oldObjective)
-                    {
-                        hasImproved = true;
-                        std::swap(paths[a1][i], paths[a2][j]);
-                        improvementSum += oldObjective - newObjective;
-                        pathLengths[a1] -= improvementA1;
-                        pathLengths[a2] -= improvementA2;
-                        longestA = newLongestA;
-                    }
-                }
-            }
-        }
-    }
-
-    return { paths, improvementSum };
-}
-
-std::tuple<std::vector<std::vector<size_t>>, double> TwoOptPaths(
-    OptimizationMode optimizationMode, std::vector<std::vector<size_t>> paths,
-    xt::xarray<double> weights, const DependencyGraph& dependencies,
-    std::chrono::steady_clock::time_point endTime)
-{
-    switch (optimizationMode)
-    {
-    case OptimizationMode::Max:
-        return TwoOptPathsMax(std::move(paths), weights, dependencies, endTime);
-    case OptimizationMode::Sum:
-        return TwoOptPathsSum(std::move(paths), weights, dependencies, endTime);
-    default:
-        throw TsplpException();
-    }
 }
 
 double CalculatePathLength(const std::vector<size_t>& path, const xt::xarray<double> weights)
@@ -544,7 +471,7 @@ double CalculatePathLength(const std::vector<size_t>& path, const xt::xarray<dou
 
 double CalculateObjective(
     const OptimizationMode optimizationMode, const std::vector<std::vector<size_t>>& paths,
-    xt::xarray<double> weights)
+    const xt::xarray<double> weights)
 {
     const auto A = paths.size();
 
@@ -555,7 +482,7 @@ double CalculateObjective(
     for (size_t a = 0; a < A; ++a)
     {
         const auto pathLength = CalculatePathLength(
-            paths[a], weights.dimension() == 2 ? weights : xt ::view(weights, a));
+            paths[a], weights.dimension() == 2 ? weights : xt::view(weights, a));
 
         switch (optimizationMode)
         {

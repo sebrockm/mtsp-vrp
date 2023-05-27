609d561980662cfa5e165937a55df2ff969969c9
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2023-05-27 10:25:42 +0200

    Tests for CalculateObjective

--- tsplp/test/HeuristicsTest.cpp
+++ tsplp/test/HeuristicsTest.cpp
@@ -24,3 +24,73 @@ TEST_CASE("path length", "[Heuristics]")
     REQUIRE(tsplp::CalculatePathLength({ 3 }, weights) == 0);
     REQUIRE(tsplp::CalculatePathLength({}, weights) == 0);
 }
+
+TEST_CASE("objective A==1", "[Heuristics]")
+{
+    // clang-format off
+    xt::xarray<double> weights =
+    {
+        { 1, 2, 3, 4 },
+        { 2, 4, 6, 8 },
+        { 4, 5, 6, 7 },
+        { 0, 1, 2, 3 }
+    };
+    // clang-format on
+
+    std::vector<std::vector<size_t>> paths1 = { { 0, 1, 2, 3 } };
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Sum, paths1, weights) == 15);
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Max, paths1, weights) == 15);
+}
+
+TEST_CASE("objective A==2 empty", "[Heuristics]")
+{
+    // clang-format off
+    xt::xarray<double> weights =
+    {
+        { 1, 2, 3, 4 },
+        { 2, 4, 6, 8 },
+        { 4, 5, 6, 7 },
+        { 0, 1, 2, 3 }
+    };
+    // clang-format on
+
+    std::vector<std::vector<size_t>> paths1 = { {} };
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Sum, paths1, weights) == 0);
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Max, paths1, weights) == 0);
+}
+
+TEST_CASE("objective A==2", "[Heuristics]")
+{
+    // clang-format off
+    xt::xarray<double> weights =
+    {
+        { 1, 2, 3, 4 },
+        { 2, 4, 6, 8 },
+        { 4, 5, 6, 7 },
+        { 0, 1, 2, 3 }
+    };
+    // clang-format on
+
+    std::vector<std::vector<size_t>> paths1 = { { 0, 1, 2, 3 }, { 0, 2, 1, 3 } };
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Sum, paths1, weights) == 15 + 16);
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Max, paths1, weights) == 16);
+}
+
+TEST_CASE("objective A==4", "[Heuristics]")
+{
+    // clang-format off
+    xt::xarray<double> weights =
+    {
+        { 1, 2, 3, 4 },
+        { 2, 4, 6, 8 },
+        { 4, 5, 6, 7 },
+        { 0, 1, 2, 3 }
+    };
+    // clang-format on
+
+    std::vector<std::vector<size_t>> paths1 = { { 0, 1, 2, 3 }, { 0, 2, 1, 3 }, { 2, 0 }, { 1 } };
+    REQUIRE(
+        tsplp::CalculateObjective(tsplp::OptimizationMode::Sum, paths1, weights)
+        == 15 + 16 + 4 + 0);
+    REQUIRE(tsplp::CalculateObjective(tsplp::OptimizationMode::Max, paths1, weights) == 16);
+}

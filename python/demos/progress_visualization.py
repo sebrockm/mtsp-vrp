8517a81d6b94805d1c3d054d4e677625d8129f33
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2023-07-28 12:05:50 +0200

    Progress visualization with both, sum and max

--- python/demos/progress_visualization.py
+++ python/demos/progress_visualization.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tsplib95 as tsplib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mtsp_vrp import solve_mtsp_vrp
 
 
-def draw_fractional_solution(fractional_values, weights, start_positions, end_positions, node_coords, name):
+def draw_fractional_solution(fractional_values, weights, mode, start_positions, end_positions, node_coords, name):
     epsilon = 1e-10
     colors = list(mcolors.BASE_COLORS.keys())
 
@@ -33,7 +34,9 @@ def draw_fractional_solution(fractional_values, weights, start_positions, end_po
     plt.switch_backend('Agg')
     plt.figure(figsize=(20, 20))
 
-    plt.annotate(f'objective: {np.sum(weights * fractional_values)}', (90, 90))
+    mode_func = getattr(np, mode)
+
+    plt.annotate(f'objective: {mode_func(np.sum(weights * fractional_values, axis=(1,2)))}', (90, 90))
 
     plt.plot(X, Y, '.')
     for s, e, c in zip(start_positions, end_positions, colors):
@@ -56,6 +59,10 @@ def draw_fractional_solution(fractional_values, weights, start_positions, end_po
     plt.savefig(name)
 
 
+skips = {'sum': 1, 'max': 100}
+
+mode = sys.argv[1]
+skip = skips[mode]
 instance = 'berlin52.tsp'
 base = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(base, '..', 'tsplib', 'tsp', instance)
@@ -66,12 +73,17 @@ N = P.dimension
 weights = np.load(file + '.weights.npy')
 
 fractionals = []
+i = 0
 def callback(fractional):
+    global i
     fractionals.append(fractional)
+    if i % skip == 0:
+        fractionals.append(fractional)
+    i += 1
 
 sp = [0, 0]
 ep = [0, 0]
-solve_mtsp_vrp(start_positions=sp, end_positions=ep, weights=weights, optimization_mode='sum', timeout=60000, fractional_callback=callback, number_of_threads=1)
+solve_mtsp_vrp(start_positions=sp, end_positions=ep, weights=weights, optimization_mode=mode, timeout=60000, fractional_callback=callback, number_of_threads=1)
 
 for i, fractional in enumerate(tqdm(fractionals)):
-    draw_fractional_solution(fractional, weights, sp, ep, P.node_coords, f'{instance}_{i}.png')
+    draw_fractional_solution(fractional, weights, mode, sp, ep, P.node_coords, f'{instance}_{mode}_{i*skip}.png')

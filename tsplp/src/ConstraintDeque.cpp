8f5869dc06f33171228d50e490df8b4d4335ad37
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2023-08-14 00:13:01 +0200

    Reduce waiting time in PopToModel

--- tsplp/src/ConstraintDeque.cpp
+++ tsplp/src/ConstraintDeque.cpp
@@ -18,15 +18,28 @@ void tsplp::ConstraintDeque::Push(LinearConstraint constraint)
 
 void tsplp::ConstraintDeque::PopToModel(size_t threadId, Model& model)
 {
-    std::unique_lock lock { m_mutex };
+    const auto constraints = [this, threadId]() -> std::vector<LinearConstraint>
+    {
+        std::unique_lock lock { m_mutex };
 
-    model.AddConstraints(m_deque.cbegin() + m_readPositions[threadId], m_deque.cend());
+        const auto prevReadPos = m_readPositions[threadId];
+        m_readPositions[threadId] = std::ssize(m_deque);
 
-    m_readPositions[threadId] = std::ssize(m_deque);
+        return { m_deque.cbegin() + prevReadPos, m_deque.cend() };
+    }();
 
-    const auto minReadPosition = *std::min_element(begin(m_readPositions), end(m_readPositions));
-    m_deque.erase(m_deque.begin(), m_deque.begin() + minReadPosition);
+    model.AddConstraints(constraints.cbegin(), constraints.cend());
 
-    for (auto& position : m_readPositions)
-        position -= minReadPosition;
+    // this step doesn't need to be performed often, so let only one thread do it
+    if (threadId == 0)
+    {
+        std::unique_lock lock { m_mutex };
+
+        const auto minReadPosition
+            = *std::min_element(begin(m_readPositions), end(m_readPositions));
+        m_deque.erase(m_deque.begin(), m_deque.begin() + minReadPosition);
+
+        for (auto& position : m_readPositions)
+            position -= minReadPosition;
+    }
 }
